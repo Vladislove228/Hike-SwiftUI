c35427b154ff2b76e0ad690947f112cdc7838c46
Vladislove228 <wlad.151@mail.ru>
2023-10-14 18:58:46 +0200

    second commit

---
+++ Hike-SwiftUI/CComponent/CustomButtonView.swift
@@ -0,0 +1,50 @@
+//
+//  CustomButtonView.swift
+//  Hike-SwiftUI
+//
+//  Created by Владислав Квинто on 14/10/2023.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(colors:[
+                        .customGreenLight,
+                        .customGreenMedium
+                    ],
+                                   startPoint:.topLeading,
+                                   endPoint: .bottomTrailing)
+                )
+            Circle()
+                .stroke(LinearGradient(colors: [
+                    .customGrayLight,
+                    .customGrayMedium],
+                                       startPoint: .top,
+                                       endPoint: .bottom),lineWidth: 4)
+            Image(systemName: "figure.hiking")
+                .fontWeight(.black)
+                .font(.system(size: 30))
+                .foregroundStyle(
+                    LinearGradient(colors: [
+                        .customGrayLight,
+                        .customGrayMedium],
+                                   startPoint: .top,
+                                   endPoint: .bottom)
+                )
+        }
+        .frame(width: 58,height: 58)
+        
+    }
+}
+
+struct CustomButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButtonView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
