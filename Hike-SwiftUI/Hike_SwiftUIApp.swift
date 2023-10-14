53151b56a7ef846ddf17bae98dff2dcfe06d8cdf
Vladislove228 <wlad.151@mail.ru>
2023-10-14 17:37:41 +0200

    first commit

---
+++ Hike-SwiftUI/Hike_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_SwiftUIApp.swift
+//  Hike-SwiftUI
+//
+//  Created by Владислав Квинто on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hike_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
